1395f5c5478e46e0b30302222f124fe7ffeb12d0
jangalanagababu42 <jangalanagababu42@gmail.com>
2022-06-04 12:36:09 +0530

    birthday reminder app

---
+++ src/components/data.js
@@ -0,0 +1,139 @@
+import avtarMale from '../image/male_avtar.png';
+import avtarFemale from '../image/female_avtar.png';
+import ayush from "../image/ayush.jpeg"
+import  anshay from "../image/anshay.jpeg"
+import hareesh from "../image/hareesh.jpeg"
+import hritvik from "../image/hritvik.jpeg"
+import nagababu from "../image/nagababu.jpeg"
+import nithish from "../image/nithish.jpeg"
+import phani from "../image/phani.jpeg"
+import prafful from "../image/prafful.jpeg"
+
+const data = [
+	
+	{
+		id: 1,
+		name: 'Jangala Nagababu',
+		dob:"04 June",
+		age: 21,
+		gender: "Male",
+	    // image: <img src={ayush} height="30px" width="30px" />
+		image: `${nagababu}`
+		// image: `${avtarMale}`		
+	},
+	{
+		id: 2,
+		name: 'Nagma Korpal',
+		dob:"21 May",
+		age: 32,
+		gender: "Female",
+		image: `${avtarFemale}`		
+	},
+	{
+		id: 3,
+		name: 'Anshu Chad',
+		dob:"03 November",
+		age: 21,
+		gender: "Male",
+		image: `${avtarMale}`		
+	},
+	{
+		id: 4,
+		name: 'anshay',
+		dob:"07 November",
+		age: 36,
+		gender: "Male",
+		image: `${anshay}`		
+	},
+	{
+		id: 5,
+		name: 'ayush',
+		dob:"06 November",
+		age: 29,
+		gender: "Male",
+		image: `${ayush}`		
+	},
+	{
+		id: 6,
+		name: 'hareesh',
+		dob:"10 October",
+		age: 22,
+		gender: "Male",
+		image: `${hareesh}`		
+	},
+	{
+		id: 7,
+		name: 'Urmila Khurana',
+		dob:"12 October",
+		age: 25,
+		gender: "Female",
+		image: `${avtarFemale}`
+	},
+	{
+		id: 8,
+		name: 'hritvik',
+		dob:"07 October",
+		age: 43,
+		gender: "Male",
+		image: `${hritvik}`
+	},
+	{
+		id: 9,
+		name: 'Amolika Maraj',
+		dob:"07 October",
+		age: 42,
+		gender: "Female",
+		image: `${avtarFemale}`
+	},
+	{
+		id: 10,
+		name: 'nithish',
+		dob:"10 October",
+		age: 46,
+		gender: "Male",
+		image: `${nithish}`
+	},
+	{
+		id: 11,
+		name: 'phani',
+		dob:"10 October",
+		age: 30,
+		gender: "Male",
+		image: `${phani}`
+	},
+	{
+		id: 12,
+		name: 'Peter Jacob',
+		dob:"12 October",
+		age: 31,
+		gender: "Male",
+		image: `${avtarMale}`
+	},
+	{
+		id: 13,
+		name: 'prafful chauhan',
+		dob:"12 October",
+		age: 20,
+		gender: "Female",
+		image: `${prafful}`
+	},
+	{
+		id: 14,
+		name: 'Jatin Ranganathan',
+		dob:"13 October",
+		age: 23,
+		gender: "Male",
+		image: `${avtarMale}`
+	},
+	{
+		id: 15,
+		name: 'Vicky Natarajan',
+		dob:"13 October",
+		age: 24,
+		gender: "Male",
+		image: `${avtarMale}`
+	},
+	
+]
+
+export default data;
